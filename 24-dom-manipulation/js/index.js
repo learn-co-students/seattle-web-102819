91a4f9699dc0761a86ffbd8e1d9eb4ee4c0da2c2
john-franti <john.franti@flatironschool.com>
2019-12-10 08:41:39 -0800

    add js 1

---
+++ 24-dom-manipulation/js/index.js
@@ -0,0 +1,38 @@
+// const arr = []
+
+// arr.push("hello")
+// arr.push("goodbye")
+
+// // def djsfhds
+// //     lkdshgljdsf
+// // end
+
+// function sayHi() {
+//     console.log("Hi")
+// }
+
+// // arr.forEach(function (word) {
+// //     console.log(word)
+// // })
+
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i])
+// }
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    let myNode = document.getElementById("salmon");
+    myNode = document.querySelector(".content")
+    myNode = document.querySelector("#salmon")
+
+    myNode.textContent = "Grilled Salmon";
+    // myNode.innerHTML = "<ul><li>Salmon</li><li>Lemon Juice</li></ul>";
+
+    console.dir(myNode)
+    
+    // myNodeList = document.querySelectorAll("div")
+    // console.log(myNodeList)
+    
+
+})
