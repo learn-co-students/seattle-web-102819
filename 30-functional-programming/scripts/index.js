636441d4b8d99c5a63610e2990ab4fe89e0b36d8
ixnp <enprocopios@gmail.com>
2019-12-19 11:02:18 -0800

    functional programming

--- 30-functional-programming/scripts/index.js
+++ 30-functional-programming/scripts/index.js
@@ -1,48 +1,46 @@
 // Functions are First Class
-const teaTime = function(){
-    return true
-} 
- 
-const notTeaTime = function(){
-    return false 
-}
+const teaTime = function() {
+  return true;
+};
 
+const notTeaTime = function() {
+  return false;
+};
 
-//functions can return other functions 
+//functions can return other functions
 
-const outSide = function(){  
-    return function(){
-        console.log('meow: let me back in')
-    }
-}
+const outSide = function() {
+  return function() {
+    console.log("meow: let me back in");
+  };
+};
 
-console.log(outSide())
-console.log(outSide()())
+console.log(outSide());
+console.log(outSide()());
 
 //functions can be passed to other functions
-let tea = 'earl grey'
-function makeTeaCallback(teaTime, tea){
-    if(teaTime()){
-        console.log(`it's tea time lets have some ${tea} and crumpets`)
-    }else{
-        console.log(`not time for tea`)
-    }
+let tea = "earl grey";
+function makeTeaCallback(teaTime, tea) {
+  if (teaTime()) {
+    console.log(`it's tea time lets have some ${tea} and crumpets`);
+  } else {
+    console.log(`not time for tea`);
+  }
 }
-makeTeaCallback(teaTime,tea)
-makeTeaCallback(notTeaTime,tea)
+makeTeaCallback(teaTime, tea);
+makeTeaCallback(notTeaTime, tea);
 
 //Where have you seen this before?
 
-
 //Arrow Functions
 
-const oneLineReturn = () => 'i am an implicit return'
-const oneVar = data => console.log(data)
-const moreThanOne = (dataOne, dataTwo) => console.log(dataOne,dataTwo)
+const oneLineReturn = () => "i am an implicit return";
+const oneVar = data => console.log(data);
+const moreThanOne = (dataOne, dataTwo) => console.log(dataOne, dataTwo);
 
-oneLineReturn()
-oneVar('one var no ()')
-moreThanOne('any more than one', 'you need ()')
+oneLineReturn();
+oneVar("one var no ()");
+moreThanOne("any more than one", "you need ()");
 
 // const moreLines = () => (
 //     console.log('this wont work')
@@ -50,67 +48,64 @@ moreThanOne('any more than one', 'you need ()')
 // )
 
 const moreLines = () => {
-    console.log('this wont work')
-    return 'welp'
-}
+  console.log("this wont work");
+  return "welp";
+};
 
 //IIFE
 
-const runNow = (function runNow(){
-    console.log('it ran')
-    return 'it ran'
-})()
+const runNow = (function runNow() {
+  console.log("it ran");
+  return "it ran";
+})();
 
 //pure functions
 
-const pureFunction = function(a,b){
-    return a + b
-}
+const pureFunction = function(a, b) {
+  return a + b;
+};
 
-const alsoPure = function(arr){
-    let oddArr = []
+const alsoPure = function(arr) {
+  let oddArr = [];
 
-    for(let i = 0; i < arr.length; i++){
-        if(arr[i]%2 !== 0 ){
-            oddArr.push(arr[i])
-        }  
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 !== 0) {
+      oddArr.push(arr[i]);
     }
-    return oddArr
-}
+  }
+  return oddArr;
+};
 
 // Mutable vs Immutable
 
-const nums = [1, 2, 3]
+const nums = [1, 2, 3];
 
-const destructivelyMutate = (numArr) => {
+const destructivelyMutate = numArr => {
   for (let i = 0; i < numArr.length; i++) {
-    numArr[i]++
+    numArr[i]++;
   }
-  return numArr
-}
+  return numArr;
+};
 
 //vs
 
-nums.map((n) => n+1) // [2, 3, 4]
+nums.map(n => n + 1); // [2, 3, 4]
 
+///building a library
 
-///building a library 
+const sum = (a, b) => a + b;
 
+function doMath(callback, secret) {
+  return function(val) {
+    return callback(secret, val);
+  };
+}
 
- const sum = (a,b) => a+b 
-
- function doMath(callback,secret){
-    return function(val){
-        return callback(secret, val)
-    }
- }
-
- const product = (a,b) => a*b
- const doubleNumber = doMath(product, 2)
- const tripleNumb = doMath(product,3)
- const add50 = doMath(sum, 50)
- console.log(doubleNumber(50))
-const add50andTriple = (x) => tripleNumb(add50(x))
-
+const product = (a, b) => a * b;
+const doubleNumber = doMath(product, 2);
+const tripleNumb = doMath(product, 3);
+const add50 = doMath(sum, 50);
+console.log(doubleNumber(50));
+const add50andTriple = x => tripleNumb(add50(x));
 
 //IIF
